68e713f17a863be9c5abbe61b5ae0cffe1c702be
tony1793 <60820793+tony1793@users.noreply.github.com>
2021-03-04 18:36:32 +0100

    Initialized home screen

--- RNproject/App.js
+++ RNproject/App.js
@@ -8,6 +8,7 @@ import JoinCreateScreen from './screens/JoinCreateScreen/JoinCreateScreen';
 import CreateApartmentScreen from './screens/CreateApartmentScreen/CreateApartmentScreen.js';
 import LoginScreen from './screens/LoginScreen/login.js';
 import JoinApartmentScreen from './screens/JoinScreen/JoinScreen';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
 
 
 const Stack = createStackNavigator();
@@ -15,13 +16,14 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="HomeScreen">
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="RegistrationScreen" component={RegistrationScreen}  />
                 <Stack.Screen name="RegistrationCompletedScreen" component={RegistrationCompletedScreen} />
                 <Stack.Screen name="JoinCreateScreen" component={JoinCreateScreen}  />
                 <Stack.Screen name="CreateApartmentScreen" component={CreateApartmentScreen}  />
                 <Stack.Screen name="JoinApartmentScreen" component={JoinApartmentScreen}  />
+                <Stack.Screen name="HomeScreen" component={HomeScreen}  />
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ RNproject/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen</Text>
+        <Text>
+          <Icon name="bug" size={30} color="#900" />
+        </Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function SchermataProva() {
+  return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            /*
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'md-browsers' : 'md-browsers';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'md-browsers' : 'md-browsers';
+            }
+            */
+            // You can return any component that you like here!
+            return <Icon name='instagram' size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      </Tab.Navigator>
+  );
+}
