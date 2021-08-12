d99f678b8c18440d907b06e644b40927deedf0d4
tony1793 <ercolaniantonio98@gmail.com>
2021-08-12 17:38:58 +0200

    Bug fixes

--- RNproject/ios/APPartmentTests/APPartmentTests.m
+++ RNproject/ios/APPartmentTests/APPartmentTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNprojectTests : XCTestCase
+@interface APPartmentTests : XCTestCase
 
 @end
 
-@implementation RNprojectTests
+@implementation APPartmentTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
